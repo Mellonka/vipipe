4e81e6b4fd5d09a82b2696fa7c01b8397cdb1836
Mellonka <facemellon@gmail.com>
2025-05-16 15:01:29 +0500

    new method cli

--- src/vipipe/transport/zeromq/utils/cli.py
+++ src/vipipe/transport/zeromq/utils/cli.py
@@ -51,3 +51,30 @@ def parse_zmq_config_cli() -> tuple[ZeroMQReaderConfig, ZeroMQWriterConfig]:
         linger=args.writer_linger,
         dontwait=args.writer_dontwait,
     )
+
+
+def parse_zmq_reader_config_cli() -> ZeroMQReaderConfig:
+    """Парсит конфигурацию ZeroMQReader из командной строки"""
+
+    parser = argparse.ArgumentParser(description="ZeroMQ reader config")
+    parser.add_argument("--reader_address", type=str, required=True, help="Socket address")
+    parser.add_argument("--reader_socket-type", type=str, default="SUB", choices=["SUB", "PULL"], help="Socket type")
+    parser.add_argument("--reader_topic", type=str, default="", help="Topic for SUB socket")
+    parser.add_argument("--reader_buffer-length", type=int, default=10, help="Buffer length")
+    parser.add_argument("--reader_buffer-size-oc", type=int, default=1024 * 1024 * 30, help="Buffer size in OS")
+    parser.add_argument("--reader_read-timeout", type=int, default=100, help="Read timeout in ms")
+    parser.add_argument("--reader_conflate", action="store_true", help="Conflate messages")
+    parser.add_argument("--reader_dontwait", action="store_true", help="Non-blocking read")
+
+    args = parser.parse_args()
+
+    return ZeroMQReaderConfig(
+        address=args.reader_address,
+        socket_type=zmq.SocketType[args.reader_socket_type],
+        topic=args.reader_topic,
+        buffer_length=args.reader_buffer_length,
+        buffer_size_os=args.reader_buffer_size_oc,
+        read_timeout=args.reader_read_timeout,
+        conflate=args.reader_conflate,
+        dontwait=args.reader_dontwait,
+    )
